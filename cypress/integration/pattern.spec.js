77eae9bba8abee8b2bfa33d6c91dbea81dbb241f
Dillon Parker <prkr93@gmail.com>
2022-04-23 18:31:47 -0600

    Refactor tests for creating dynamic pattern on page load

--- cypress/integration/pattern.spec.js
+++ cypress/integration/pattern.spec.js
@@ -1,13 +1,18 @@
 describe('Pattern', () => {
-  it.skip('should automatically create a grid upon visiting the url', () => {
-    cy.visit('http://localhost:3000/5x5')
-      .get('section.pattern')
-      .children('section.row').should('have.length', 5)
-      .children('article.stitch').should('have.length', 25) //this might also be 5?
+  it('should automatically create a grid upon visiting the url', () => {
+    cy.visit('http://localhost:3000/create/5x5')
+      .get('section.pattern').within(pattern => {
+        cy.get('section.row').should('have.length', 5)
+          .find('article.stitch').should('have.length', 25) //this might also be 5?
+      })
   });
 
-  it.skip('should be able to have a dynamic pattern size', () => {
-    cy.visit('http://localhost:3000/9x9')
+  it('should be able to have a dynamic pattern size', () => {
+    cy.visit('http://localhost:3000/create/9x9')
+      .get('section.pattern').within(pattern => {
+        cy.get('section.row').should('have.length', 9)
+          .find('article.stitch').should('have.length', 81) //this might also be 5?
+      })
   });
 
   it.skip('should be able to have another dynamic pattern size', () => {

0901ed13a262e3e836fbefb843885b7fc5a89242
Dillon Parker <prkr93@gmail.com>
2022-04-23 18:38:29 -0600

    Finish tests for pattern error handling and dynamic pattern

--- cypress/integration/pattern.spec.js
+++ cypress/integration/pattern.spec.js
@@ -3,7 +3,7 @@ describe('Pattern', () => {
     cy.visit('http://localhost:3000/create/5x5')
       .get('section.pattern').within(pattern => {
         cy.get('section.row').should('have.length', 5)
-          .find('article.stitch').should('have.length', 25) //this might also be 5?
+          .find('article.stitch').should('have.length', 25)
       })
   });
 
@@ -11,18 +11,21 @@ describe('Pattern', () => {
     cy.visit('http://localhost:3000/create/9x9')
       .get('section.pattern').within(pattern => {
         cy.get('section.row').should('have.length', 9)
-          .find('article.stitch').should('have.length', 81) //this might also be 5?
+          .find('article.stitch').should('have.length', 81)
       })
   });
 
-  it.skip('should be able to have another dynamic pattern size', () => {
-    cy.visit('http://localhost:3000/1x9')
+  it('should be able to have another dynamic pattern size', () => {
+    cy.visit('http://localhost:3000/create/1x9')
+      .get('section.pattern').within(pattern => {
+        cy.get('section.row').should('have.length', 1)
+          .find('article.stitch').should('have.length', 9)
+      })
   });
 
-  it.skip('should redirect if entered an invalid url', () => {
-    cy.visit('http://localhost:3000/0x5', {failOnStatusCode: false})
-      .url().should('be.equals', 'http://localhost:3000/')
-      
+  it('should redirect if entered an invalid url', () => {
+    cy.visit('http://localhost:3000/create/0x5', {failOnStatusCode: false})
+      .url().should('be.equals', 'http://localhost:3000/create')
   });
   
 });
